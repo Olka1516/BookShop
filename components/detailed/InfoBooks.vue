048f044ee92f2192db9be99e5519cfaaf799520e
Olka1516 <olkakorolcuk@gmail.com>
2024-06-24 01:33:25 +0300

    Done with landing page

--- components/detailed/InfoBooks.vue
+++ components/detailed/InfoBooks.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="container">
+  <div class="info-content">
     <img class="book-img" :src="`${props.image}`" alt="" />
     <div class="info-container">
-      {{ props.title }}
+      <div class="info-texts">
+        <h1>
+          {{ props.title }}
+        </h1>
+        <p>{{ props.description }}</p>
+      </div>
+      <NuxtLink to="/about" class="no-bg-no-border">Details</NuxtLink>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
+  id: string;
   title: string;
   image: string;
   description: string;
