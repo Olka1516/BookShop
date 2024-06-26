c906e9f933d763ca53251d6293651453a2ea5eed
Olka1516 <olkakorolcuk@gmail.com>
2024-06-26 17:37:23 +0300

    Connect backend and frontend

--- pages/details/[id].vue
+++ pages/details/[id].vue
@@ -1,23 +1,23 @@
 <template>
   <div class="details">
     <div class="details-line"></div>
-    <img class="details-book-img" src="/temp/1.jpg" alt="" />
+    <img class="details-book-img" :src="`/temp/${dataFetch.image}`" alt="" />
     <div class="details-content">
       <div class="details-content-texts">
         <div>
-          <h1>{{ data.title }}</h1>
-          <h2>{{ data.author }}</h2>
+          <h1>{{ dataFetch.title }}</h1>
+          <h2>{{ dataFetch.author }}</h2>
         </div>
         <div class="details-stars">
           <div class="details-star" v-for="n in 5">
-            <img v-if="data.stars >= n" src="/star-full.svg" alt="" />
+            <img v-if="dataFetch.star >= n" src="/star-full.svg" alt="" />
             <img v-else src="/star.svg" alt="" />
           </div>
         </div>
-        <p>{{ data.description }}</p>
+        <p>{{ dataFetch.description }}</p>
       </div>
       <div class="details-buy">
-        <h2>${{ data.price }}</h2>
+        <h2>${{ dataFetch.price }}</h2>
         <button>Buy</button>
       </div>
     </div>
@@ -26,17 +26,17 @@
 
 <script setup lang="ts">
 const route = useRoute();
-
-const data = reactive({
-  id: "2",
-  title: "Book about unicorns",
-  author: "Stephan King",
-  stars: 3,
-  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur veniam aliquid ad porro odit doloremque ex culpa totam dolor explicabo, quis quia maxime nisi id eligendi aspernatur nihil, neque commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur veniam aliquid ad porro odit doloremque ex culpa totam dolor explicabo, quis quia maxime nisi id eligendi aspernatur nihil, neque commodi. 
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur veniam aliquid ad porro odit doloremque ex culpa totam dolor explicabo, quis quia maxime nisi id eligendi aspernatur nihil, neque commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur veniam aliquid ad porro odit doloremque ex culpa totam dolor explicabo, quis quia maxime nisi id eligendi aspernatur nihil, neque commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur veniam aliquid ad porro odit doloremque ex culpa totam dolor explicabo, quis quia maxime nisi id eligendi aspernatur nihil, neque commodi.`,
-  image: "/temp/2.jpg",
-  price: "24",
-});
+const dataFetch: {
+  id: string;
+  title: string;
+  description: string;
+  author: string;
+  price: number;
+  count: string;
+  image: string;
+  category: string;
+  star: number;
+} = await $fetch("http://localhost:8080/api/book/get-book/" + route.params.id);
 </script>
 
 <style scoped lang="scss">
