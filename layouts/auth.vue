2c997b6ac5fa863a6abf2d10987b7cfd3fa068e1
Olka1516 <olkakorolcuk@gmail.com>
2024-07-06 19:42:20 +0300

    BK-8: Create sign in and sign up pages

---
+++ layouts/auth.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img src="/books.png" alt="" />
+
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@use "@/assets/index";
+@include index.auth;
+</style>
---
+++ pages/signUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- <img src="/books.png" alt="" /> -->
+     
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "auth",
+});
+</script>
+
+<style scoped lang="scss">
+@use "@/assets/index";
+@include index.sign-up;
+</style>
