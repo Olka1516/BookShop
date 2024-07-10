bc89999d8640bccb6c9425acbd12cbd19fc8fcbb
Olka1516 <olkakorolcuk@gmail.com>
2024-07-10 18:34:15 +0300

    BK-6: Create function for delete books from local storage

--- components/basket/Table.vue
+++ components/basket/Table.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
     <h1>Shopping card items</h1>
-    <div class="table">
-      <div v-for="book in data" class="table-row">
-        <BasketTableCard :book="book" />
+    <div class="table" v-if="newData.length">
+      <div v-for="book in newData" class="table-row">
+        <BasketTableCard :book="book" @deleteBook="(id) => deleteBook(id)" />
       </div>
+    </div>
+    <div v-else class="table">
+      <h2 class="table-row table-empty">You do not choose your books</h2>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { data } from "~/mocks/mock";
+import type { Book } from "~/types";
+
+const newData = ref([]);
+onMounted(() => {
+  const data = localStorage.getItem("basket");
+  newData.value = data ? JSON.parse(data) : [];
+});
+
+const deleteBook = (id: string) => {
+  newData.value = newData.value.filter((book: Book) => book.id !== id);
+};
 </script>
 
 <style scoped lang="scss">
