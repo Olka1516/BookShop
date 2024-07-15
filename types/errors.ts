a3f640ac27b4806f4520925ebc2555afb74853b5
Olka1516 <olkakorolcuk@gmail.com>
2024-07-15 17:49:49 +0300

    BK-10: Created connecting authentication requests

--- types/errors.ts
+++ types/errors.ts
@@ -1,11 +1,18 @@
 export enum ErrorMessageEnum {
-    IsRequired = "Value is required",
-    EmailIsNotValid = "Value is not a valid email address",
-    EmailInUse = "Error: Email is already in use!",
-    UsernameInUse = "Error: Username is already taken!",
-    PasswordMinLength = "This field should be at least 6 characters long",
-    PasswordIsEqual = "The value must be equal to the other value",
-    UsernameOrPasswordWrong = "Bad credentials",
-    MaxLength50 = "The maximum length allowed is 50",
-    MaxLength255 = "The maximum length allowed is 255",
-  }
+  IsRequired = "Value is required",
+  EmailIsNotValid = "Value is not a valid email address",
+  EmailInUse = "Error: Email is already in use!",
+  UsernameInUse = "Error: Username is already taken!",
+  PasswordMinLength = "This field should be at least 6 characters long",
+  PasswordIsEqual = "The value must be equal to the other value",
+  UsernameOrPasswordWrong = "Bad credentials",
+  MaxLength50 = "The maximum length allowed is 50",
+  MaxLength255 = "The maximum length allowed is 255",
+}
+
+export interface TRequestError {
+  data: {
+    message: string;
+    status: number;
+  };
+}
